c7c0930acf3a9fcf33db17cf79ea88f825e86dc5
Andrew Schmit <schmita91@gmail.com>
2021-01-07 12:49:40 -0500

    patch endpoints and more tests

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const xss = require('xss')
 const ArticlesService = require('./articles-service')
+const path = require('path');
+
 
 const articlesRouter = express.Router()
 const jsonParser = express.json()
@@ -40,7 +42,7 @@ articlesRouter
       .then(article => {
         res
           .status(201)
-          .location(`/articles/${article.id}`)
+          .location(path.posix.join(req.originalUrl, `/${article.id}`))
           .json(serializeArticle(article))
       })
       .catch(next)
@@ -76,6 +78,30 @@ articlesRouter
         res.status(204).end()
       })
       .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+      const { title, content, style } = req.body
+      const articleToUpdate = { title, content, style }
+
+      const numberOfValues = Object.values(articleToUpdate).filter(Boolean).length
+      if (numberOfValues === 0) {
+          return res.status(400).json({
+              error: {
+                    message: `Request body must contain either 'title', 'style' or 'content'`
+              }
+          })
+      }
+    
+
+      ArticlesService.updateArticle(
+          req.app.get('db'),
+          req.params.article_id,
+          articleToUpdate
+      )
+        .then(numRowsAffected => {
+            res.status(204).end()
+        })
+        .catch(next)
   })
 
 module.exports = articlesRouter
